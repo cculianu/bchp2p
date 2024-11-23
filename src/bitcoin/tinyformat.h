9201582d52ccde4c081802f7d4de87540ada947a
Calin Culianu <calin.culianu@gmail.com>
2024-11-24 00:04:22 +0100

    Minor tweak: Constrain strprintf fmt string to be convertible-to string_view
    Using C++20 concepts

--- src/bitcoin/tinyformat.h
+++ src/bitcoin/tinyformat.h
@@ -1,12 +1,13 @@
 #pragma once
 #include <fmt/printf.h>
 
+#include <concepts>
 #include <string_view>
 #include <utility>
 
 namespace bitcoin {
 
-template <typename ...Args>
-auto strprintf(std::string_view fmt, Args &&...args) { return fmt::sprintf(fmt, std::forward<Args>(args)...); }
+template <std::convertible_to<std::string_view> S, typename ...Args>
+auto strprintf(const S & fmt, Args &&...args) { return fmt::sprintf(fmt, std::forward<Args>(args)...); }
 
 } // namespace bitcoin
